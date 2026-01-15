b8aae648533af51da99d771c176e3e90b9db0614
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-15 22:25:28 +0600

    Add files via upload

---
+++ Day-29.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+
+// If else whole topic revise
+
+// 1.Take an integer and check whether it is positive, negative, or zero.
+
+int n;
+scanf("%d",&n);
+
+if(n>0){
+    printf("Number is positive");
+}
+else if(n==0){
+    printf("Number is zero");
+}
+else{
+    printf("Number is negative");
+}
+
+
+
+
+    return 0;
+}
