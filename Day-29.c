daa3f51c972777ae7cbf496cd1c49a5dfe5f33b1
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-15 22:28:09 +0600

    Update Day-29.c

--- Day-29.c
+++ Day-29.c
@@ -22,5 +22,26 @@ else{
 
 
 
+
+
+
+// 2.Input a number and check if it is divisible by both 5 and 11.
+
+int n;
+scanf("%d",&n);
+
+if(n%5==0 && n%11==0){
+    printf("N is divisible by both 5 and 11");
+}
+else{
+    printf("Not divisible");
+}
+
+
+
+    
+
+
     return 0;
+
 }
